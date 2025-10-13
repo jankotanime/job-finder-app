e7d260134fe0028ef06b985f16bc020e5507bc5e
jankotanime <jan.gasztold@gmail.com>
2025-10-13 14:53:47 +0200

    init: spring-boot-service with health check See #5

---
+++ spring-boot-service/src/main/java/com/mimaja/job_finder_app/global/Healthcheck.java
@@ -0,0 +1,15 @@
+package com.mimaja.job_finder_app.global;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health-check")
+public class Healthcheck {
+  @GetMapping
+  public ResponseEntity<String> getHealthcheck() {
+    return ResponseEntity.ok("Application is running");
+  }
+}
