f4cf1200237863dd13ff54b8782d3aaa16b01e32
madamski9 <maciek.adamski9@gmail.com>
2025-10-16 23:22:11 +0200

    add: theme constants for dark mode, async storage for saving user theme state

--- mobile/src/contexts/ThemeContext.tsx
+++ mobile/src/contexts/ThemeContext.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
-import { MD3LightTheme as DefaultTheme, MD3DarkTheme as DarkTheme, PaperProvider } from 'react-native-paper'
+import React, { useState, useEffect } from 'react'
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper'
 import { createContext, useContext } from 'react'
 import { useColorScheme } from 'react-native'
+import { themeDark, themeLight } from '../constans/theme'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 type ThemeContextType = {
     theme: typeof DefaultTheme,
@@ -10,9 +12,7 @@ type ThemeContextType = {
 }
 const defaultTheme: ThemeContextType = {
     theme: DefaultTheme,
-    toggleTheme: () => {
-        console.warn("useTheme hook called outside ThemeProvider")
-    },
+    toggleTheme: () => {},
     isDarkMode: false
 }
 const ThemeContext = createContext<ThemeContextType>(defaultTheme)
@@ -20,9 +20,32 @@ const ThemeContext = createContext<ThemeContextType>(defaultTheme)
 export const ThemeProvider = ({ children }: { children: React.ReactNode}) => {
     const systemTheme = useColorScheme()
     const [isDarkMode, setIsDarkMode] = useState<boolean>(systemTheme === 'dark')
-    const toggleTheme = () => setIsDarkMode(prev => !prev)
-    const theme = isDarkMode ? DarkTheme : DefaultTheme
+    const theme = isDarkMode ? themeDark : themeLight
+    const [isLoading, setIsLoading] = useState<boolean>(true)
 
+    useEffect(() => {
+        const loadTheme = async () => {
+            try {
+                const userTheme = await AsyncStorage.getItem("isDarkMode")
+                if (userTheme !== null) setIsDarkMode(JSON.parse(userTheme))
+            } catch (e) {
+                console.error("error during saving theme: ", e)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        loadTheme()
+    }, [theme])
+    const toggleTheme = async () => {
+        try {
+            const newThemeState = !isDarkMode
+            setIsDarkMode(newThemeState)
+            await AsyncStorage.setItem("isDarkMode", JSON.stringify(newThemeState))
+        } catch (e) {
+            console.error("error during toggling theme: ", e)
+        }
+    }
+    if (isLoading) return null
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme, isDarkMode}}>
             <PaperProvider theme={theme}>
