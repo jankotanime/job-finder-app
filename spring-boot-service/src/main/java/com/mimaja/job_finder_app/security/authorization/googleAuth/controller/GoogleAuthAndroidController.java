5ee0378eafb92d0dc3460351c07bb3c4e4923cc6
Jan Gasztold <119004677+jankotanime@users.noreply.github.com>
2025-11-24 14:17:31 +0100

    Fix typo in GoogleAuthAndroidController mapping

--- spring-boot-service/src/main/java/com/mimaja/job_finder_app/security/authorization/googleAuth/controller/GoogleAuthAndroidController.java
+++ spring-boot-service/src/main/java/com/mimaja/job_finder_app/security/authorization/googleAuth/controller/GoogleAuthAndroidController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth/google-auth/andriod")
+@RequestMapping("/auth/google-auth/android")
 public class GoogleAuthAndroidController {
     private final GoogleAuthServiceAndroid googleAuthServiceAndroid;
 
